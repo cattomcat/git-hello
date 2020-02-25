82f035d0280855e5bf2d6de2b7ff425b49a2ff0f
cattomcat <864644476@qq.com>
2020-02-25 23:59:35 +0800

    V0.0

---
+++ src/main/java/pers/shane/Example.java
@@ -0,0 +1,20 @@
+package pers.shane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Example {
+    @RequestMapping("/")
+    String home(){
+        return "hello git";
+    }
+
+    public static void main(String[] args) throws Exception{
+        SpringApplication.run(Example.class);
+    }
+
+}
