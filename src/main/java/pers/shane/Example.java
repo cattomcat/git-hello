798d86803320181a1c4ce6c6435ac61f1082033d
cattomcat <864644476@qq.com>
2020-02-26 00:34:48 +0800

    master V0.3

--- src/main/java/pers/shane/Example.java
+++ src/main/java/pers/shane/Example.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Example {
     @RequestMapping("/")
     String home(){
-        return "hello git V0.1";
+        return "hello git master V0.3";
     }
 
     public static void main(String[] args) throws Exception{
